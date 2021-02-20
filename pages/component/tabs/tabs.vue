62d1f53c32c997c948780b076480d8d4ce8e7fec
linshaohui <linshaohui@kuaishou.com>
2021-02-20 16:13:15 +0800

    feat:失物招领

---
+++ pages/component/tabs/tabs.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="tab-list-container">
+    <div class="tab-list">
+      <div
+        v-for="(item, index) in tabsList"
+        :key="index"
+        @click="$emit('clickTab', index)"
+      >
+        <!-- tab-nav-name -->
+        <span class="tab-name">{{ item.tabName }}</span>
+        <!-- tab-nav-block -->
+        <span
+          :style="{
+            background:
+              index == activeTabIndex ? '#4402ec' : 'transparent',
+          }"
+          class="tab-block"
+        />
+      </div>
+    </div>
+    <div class="card-list">
+      <div
+        v-for="(item, index) in showDatasource"
+        :key="index"
+        class="card-item"
+      >
+        <div class="card-top">
+          <div class="card-top-left">
+            <img 
+              :src="item.imgPath" 
+              alt="" >
+          </div>
+          <div class="card-top-right">
+            <h4>{{ item.titleConfig.value }}</h4>
+            <div>{{ item.content.value }}</div>
+          </div>
+        </div>
+        <div class="card-bottom">
+          <div class="icon-publisher">
+            <uni-icons 
+              class="icon" 
+              type="contact" 
+              size="24" />
+            <span class="publisher-name">{{
+              item.publisher.value
+            }}</span>
+          </div>
+          <span class="publish-date">{{
+            item.publishDate.value
+          }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import image from '../../API/image/image.vue';
+export default {
+	components: { image },
+	props: {
+		tabsList: {
+			type: Array,
+			default: () => [],
+		},
+		showDatasource: {
+			type: Array,
+			default: () => [],
+		},
+		activeTabIndex: {
+			type: Number,
+			default: 0,
+		},
+	},
+	data() {
+		return {};
+	},
+	created() {
+		// console.log(this.props.tabsList);
+	},
+};
+</script>
+
+<style lang="less" scope>
+@import "./tabs.less";
+</style>
---
+++ pages/lostAndFound/lostAndFound.vue
@@ -0,0 +1,106 @@
+<template>
+	<div class="lost-found-container">
+		<tabs
+			:tabs-list="tabsList"
+			:show-datasource="showDatasource"
+			:active-tab-index="activeTabIndex"
+			@clickTab="clcikTab"
+		/>
+	</div>
+</template>
+
+<script>
+import tabs from "pages/component/tabs/tabs.vue";
+export default {
+	name: "HelloWorld",
+	components: { tabs },
+	data() {
+		return {
+			showDatasource: [],
+			activeTabIndex: 0,
+			tabsList: [
+				{
+					tabName: "寻找失物",
+					dataSource: [
+						{
+							imgPath: "../../static/image/activity-bgc.png",
+							titleConfig: {
+								style: {},
+								value: "寻找一双鞋子",
+							},
+							content: {
+								style: {},
+								value:
+									"我的aj房子家门口突然就不见了，大家帮帮忙我的aj房子家门口突然就不见了，大家帮帮忙，救救孩子可以我的aj房子家门口突然就不见了，大家帮帮忙我的aj房子家门口突然就不见了，大家帮帮忙，救救孩子可以",
+							},
+							publisher: {
+								style: {},
+								value: "发布者名字",
+							},
+							publishDate: {
+								style: {},
+								value: "2021-1-2",
+							},
+						},
+						{
+							imgPath: "../../static/image/activity-bgc.png",
+							titleConfig: {
+								style: {},
+								value: "寻找一双鞋子",
+							},
+							content: {
+								style: {},
+								value:
+									"我的aj房子家门口突然就不见了，大家帮帮忙，救救孩子可以",
+							},
+							publisher: {
+								style: {},
+								value: "发布者名字",
+							},
+							publishDate: {
+								style: {},
+								value: "2021-1-2",
+							},
+						},
+					],
+				},
+				{
+					tabName: "寻找失主",
+					dataSource: [
+						{
+							imgPath: "",
+							titleConfig: {
+								style: {},
+								value: "",
+							},
+							content: {
+								style: {},
+								value: "",
+							},
+							publisher: {
+								style: {},
+								value: "",
+							},
+							publishDate: {
+								style: {},
+								value: "2021-1-2",
+							},
+						},
+					],
+				},
+			],
+		};
+	},
+	created() {
+		this.showDatasource = this.tabsList[0].dataSource;
+	},
+	methods: {
+		// 点击tab-nav-name进行切换
+		clcikTab(i) {
+			this.activeTabIndex = i;
+			this.showDatasource = this.tabsList[i].dataSource;
+			console.log(this.activeTabIndex);
+		},
+	},
+};
+</script>
